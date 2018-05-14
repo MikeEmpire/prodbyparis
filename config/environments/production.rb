8903d3f68f8048acafb30c8aac4fff7b4dd46e6c
Michael Olie <aolie94@gmail.com>
2018-05-14 08:37:41 -0700

    added serve assets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -59,8 +59,9 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "prodbyparis_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "beachgoon--website_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.serve_static_assets = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
